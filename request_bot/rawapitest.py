17293eee32b961ee2f159e8f1db9361dda2cde03
CCIbramGaunt <yury.popovskiy@gmail.com>
2025-04-14 17:54:19 +0300

    made an echo bot on asyncio. stuck with ssl certificate verification

---
+++ request_bot/rawapitest.py
@@ -0,0 +1,43 @@
+import asyncio
+import aiohttp
+import certifi
+TOKEN = '7633105953:AAHf383wggpu0PSz7WtfiRbxpAbGffJNcjE'
+URL = f'https://api.telegram.org/bot{TOKEN}/'
+
+async def send_message(chat_id, text):
+	async with aiohttp.ClientSession() as session:
+		params = {'chat_id': chat_id, 'text': text}
+		async with session.post(URL + 'sendMessage', data = params, verify = certifi.where()) as response:
+			await response.json()
+
+async def handle_updates(update):
+	message = update.get('message', False)
+	if message:
+		chat_id = message['chat']['id']
+		text = message.get('text', False)
+		if text:
+			await send_message(chat_id, f'Эхо: {text}')
+		else:
+			await send_message(chat_id, f'Я работаю только с текстом')
+
+async def get_updates():
+	offset = None
+	async with aiohttp.ClientSession() as session:
+		while True:
+			params = {'timeout':10, 'offset': offset}
+			async with session.post(URL + 'getUpdates', data = params) as response:
+				updates = await response.json()
+				if len(updates['result']) > 0:
+					offset = updates['result'][-1]['update_id'] + 1
+					for update in updates['result']:
+						await handle_updates(update)
+
+						# for_print = update.copy()
+						# for_print['message']['from']['id'] = -359689323
+						# for_print['message']['chat']['id'] = -7633105953
+						# print(for_print)
+
+async def main():
+	await get_updates()
+
+asyncio.run(main())
